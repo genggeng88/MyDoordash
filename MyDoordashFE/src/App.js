84de84a776e3477322ac974b22a0809317177411
Qin Geng <gengqin50@gmail.com>
2026-02-10 13:36:16 -0800

    new doordash frontend skeleton

---
+++ MyDoordashFE/src/App.js
@@ -0,0 +1,22 @@
+import { Layout } from "antd";
+
+const { Header, Content } = Layout;
+
+function App() {
+  return (
+    <Layout style={{ height: "100vh" }}>
+      <Header style={{ color: "white" }}>header</Header>
+      <Content
+        style={{
+          padding: "50px",
+          maxHeight: "calc(100% - 64px",
+          overflowY: "auto",
+        }}
+      >
+        content
+      </Content>
+    </Layout>
+  );
+}
+
+export default App;
